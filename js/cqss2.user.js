d78d516b3761c25b68d54a74be324e4dc468c708
oqo0opo <23419458+oqo0opo@users.noreply.github.com>
2025-12-16 21:51:45 +0800

    Create cqss2.user.js

---
+++ js/cqss2.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        游戏多功能工具箱
+// @namespace   http://tampermonkey.net/
+// @version     0.55
+// @description 最终版，优化UI布局和日志，修复所有已知问题。
+// @match       *://wan.baidu.com/game*
+// @match       *://cqss2login.bigrnet.com/baidu*
+// @updateURL   https://wget.la/https://raw.githubusercontent.com/oqo0opo/filehub/main/js/cqss2.user.js
+// @downloadURL https://wget.la/https://raw.githubusercontent.com/oqo0opo/filehub/main/js/cqss2.user.js
+// @grant       none
+// ==/UserScript==
+
+(function() {
+}
+  
